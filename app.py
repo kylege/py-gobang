ba8669952004ac20a2851a945c41859920cd9977
hichenming <hichenming@gmail.com>
2013-01-10 22:31:23 +0800

    use websocket instead of comet

--- app.py
+++ app.py
@@ -3,19 +3,15 @@
 
 #用apt-get install python-tornado安装的不是最新，有bug。要用 pip install tornado
 #
-#此方案采用http长连接与zeromq实现
+#此方案采用websocket实现
 
 import os
 from Gobang import Gobang, GameRoom
 import random
 
-import zmq
-from zmq.eventloop import zmqstream, ioloop
-ioloop.install()
-
 import tornado
 import json
-from tornado import web, autoreload, websocket
+from tornado import web, autoreload, websocket, ioloop
 from Config import Config
 from datetime import timedelta
 
@@ -23,236 +19,134 @@ from datetime import timedelta
     进入房间页面
 '''
 class EnterRoomHandler(web.RequestHandler):
+
     def get(self, room_name):
 
-        if not room_name in all_rooms.keys(): #新房间
+        if not room_name in GameSocketHandler.all_rooms.keys(): #新房间
             piece_id = random.randint(1,2)
             room = GameRoom(room_name, piece_id)
             self.render('index.html', cur_room=room, my_piece_id=piece_id, is_waiting=True,
-                all_rooms_count=len(all_rooms)+1,
+                all_rooms_count=len(GameSocketHandler.all_rooms)+1,
                 config=Config,
                 )
-            if isLog:  print '第一个进入'
         else:
-            if len(all_rooms[room_name].user_piece_ids) == 2: #已经满了
+            if len(GameSocketHandler.all_rooms[room_name].user_piece_ids) == 2: #已经满了
                 readonly = True
                 if isLog:  print '房间已被占用'
-                self.render('msg.html', msg="房间已被占用", config=Config,all_rooms_count=len(all_rooms),)
+                self.render('msg.html', msg="房间已被占用", config=Config,
+                    all_rooms_count = len(GameSocketHandler.all_rooms),)
                 return
-            elif len(all_rooms[room_name].user_piece_ids) == 1:
-                my_piece_id = (1 in all_rooms[room_name].user_piece_ids and 2 or 1)
-                all_rooms[room_name].user_piece_ids.add(my_piece_id)
+            elif len(GameSocketHandler.all_rooms[room_name].user_piece_ids) == 1:
+                his_piece_id = 1 in GameSocketHandler.all_rooms[room_name].user_piece_ids and 1 or 2
+                my_piece_id = his_piece_id == 1 and 2 or 1
+                hiskey = room_name+'-'+str(his_piece_id)
+                GameSocketHandler.all_rooms[room_name].user_piece_ids.add(my_piece_id)
 
-                all_rooms[room_name].status = GameRoom.STATUS_GOING
+                GameSocketHandler.all_rooms[room_name].status = GameRoom.STATUS_GOING
                 topic = (room_name+''+str(my_piece_id)).encode('UTF-8')
-                ioloop.IOLoop.instance().add_timeout(timedelta(seconds=1), 
-                    lambda:self._pubContentCallBack([topic,'start,']))
-                self.render('index.html', cur_room=all_rooms[room_name], my_piece_id=my_piece_id, is_waiting=(my_piece_id==2), 
-                    all_rooms_count=len(all_rooms),
-                    config=Config
+                ioloop.IOLoop.instance().add_timeout(timedelta(seconds=1),
+                    lambda:GameSocketHandler.socket_handlers[hiskey].write_message({'type':'on_gamestart'}) )
+                self.render('index.html', cur_room=GameSocketHandler.all_rooms[room_name],
+                    my_piece_id = my_piece_id,
+                    is_waiting = (my_piece_id==2),
+                    all_rooms_count = len(GameSocketHandler.all_rooms),
+                    config = Config,
                     )
                 if isLog:  print '游戏开始'
 
-    def _pubContentCallBack(self, content):
-        if isLog: print 'Enter room msg'
-        pubContent(content)
 
 '''
-    下棋
-'''
-class GameStepHandler(web.RequestHandler):
-    
-    def post(self):
-        pos = self.get_argument('pos')
-        if not pos:
-            self.write({'result':False, 'msg':'pos is empty'})
-            self.finish()
-        posarr = pos.split(',')
-        if not posarr or not len(posarr) == 2:
-            self.write({'result':False, 'msg':'pos is invalid'})
-            self.finish()
-
-        room_name = self.get_argument('room')
-        if not room_name in all_rooms.keys(): 
-            self.write({'result':False, 'msg':'room not exists'})
-            self.finish()
-        if not all_rooms[room_name].status == GameRoom.STATUS_GOING:
-            self.write({'result':False, 'msg':'room not going'})
-            self.finish()
-        user_piece = self.get_argument('up')
-        if not user_piece:
-            self.write({'result':False, 'msg':'identity not exists'})
-            self.finish()
-
-        ret = all_rooms[room_name].gobang.addPiece(int(posarr[0]), int(posarr[1]), int(user_piece))
-        if not ret.result:
-            self.write({'result':False, 'msg':ret.msg})
-        else:
-            if all_rooms[room_name].gobang.isGameOver(int(posarr[0]), int(posarr[1])):
-                if isLog:  print room_name.encode('UTF-8')+' 游戏结束'
-                pubContent([(room_name+'1').encode('UTF-8'), ('end,'+user_piece+','+pos).encode('UTF-8')])
-                pubContent([(room_name+'2').encode('UTF-8'), ('end,'+user_piece+','+pos).encode('UTF-8')])
-                all_rooms[room_name].status = GameRoom.STATUS_END
-                if isLog:  print '清空房间'+room_name.encode('UTF-8')
-                del all_rooms[room_name]
-            else:
-                pubContent([(room_name+''+user_piece).encode('UTF-8'), pos.encode('UTF-8')])
-            self.write({'result':True})
-
-
-'''
-    长连接拿对方下棋位置
-'''
-class GamePollHandler(web.RequestHandler):
-
-    @tornado.web.asynchronous
-    def get(self):
-        self.room_name = self.get_argument('room')
-        self.user_piece = self.get_argument('up')
-        if not self.user_piece or not self.room_name:
-            self.write({'result':False, 'msg':'user not valid'})
-            self.finish()
-            return
-        self.his_piece = self.user_piece == '1' and '2' or '1'
-
-        topic = self.room_name+''+self.his_piece
-        if isLog:  print u'订阅: '+topic.encode('utf-8')
-        ctx = zmq.Context.instance()
-        s = ctx.socket(zmq.SUB)
-        s.connect(zmq_addr)
-        s.setsockopt(zmq.SUBSCRIBE, topic.encode('utf-8'))
-        self.stream = zmqstream.ZMQStream(s)
-
-        self.stream.on_recv(self._handle_reply)
-        return
-
-    def _handle_reply(self, msg):
-        if self.request.connection.stream.closed():
-            return
-        if isLog: print 'Recieve: '+msg[0]+' '+msg[1]
-        reply = msg[1]
-        try:
-            self.write({'result':True, 'code':2, 'data':reply}) # code 1:game start, code 2:game step
-            self.stream.stop_on_recv()
-            self.stream.close()
-        except:
-            print 'exception poll reply'
-            pass 
-        self.finish()
-
-    def on_connection_close(self):
-        try:
-            self.stream.stop_on_recv()
-            self.stream.close()
-        except:
-            print 'exception poll connection close'
-            pass
-'''
-    保持一个长连接，断开就当离线
-'''
-class GameAliveHandler(web.RequestHandler):
-
-    @web.asynchronous
-    def get(self):
-
-        if isLog: print 'Enter alive'
-        self.room_name = self.get_argument('room')
-        self.user_piece = self.get_argument('up')
-        if not self.room_name or not self.user_piece:
-            self.write('not up or not rn')
-            self.finish()
-            return
-        if not self.room_name in all_rooms.keys(): #新房间
-            room = GameRoom(self.room_name, int(self.user_piece))
-            all_rooms[self.room_name] = room
-        return
-
-
-    def on_connection_close(self):
-        if isLog: print 'Alive连接断开'
-        removeUserFromRoom(self.room_name, [int(self.user_piece)])
-
-'''
-    移除指定房间指定用户
-'''
-def removeUserFromRoom(room_name, user_pieces):
-    if not room_name in all_rooms.keys():
-        return False
-    if isLog: print ('移除用户:'+','.join([str(u) for u in user_pieces]))
-
-    for piece in user_pieces:
-        if piece in all_rooms[room_name].user_piece_ids:
-            all_rooms[room_name].user_piece_ids.remove(piece)
-            if isLog:  print room_name.encode('utf-8') + '' + str(piece) + '离线'
-            pubContent([room_name.encode('utf-8')+''+str(piece), 'off,'])
-            his_piece = piece == 1 and 2 or 1
-            pubContent([room_name.encode('utf-8')+str(his_piece), 'end,0,-1,-1'])
-
-    all_rooms[room_name].gobang.pieces = [([0] * (Gobang.GRID_SIZE+1)) for i in range(Gobang.GRID_SIZE+1)]
-    all_rooms[room_name].gobang.last_piece = None
-    if not all_rooms[room_name].user_piece_ids:
-        if isLog: print '移除房间:'+room_name.encode('utf-8')
-        del all_rooms[room_name]
-    return True
-
-''' 
     用websocket来与前端通信
 '''
 class GameSocketHandler(tornado.websocket.WebSocketHandler):
+    
     socket_handlers = {}   #房间名-1:GameSocketHandler 一个房间每个人有一个值, 1用户订阅 room-1
+    all_rooms = {}  # 房间名:GameRoom
 
     def open(self):
-        try:
-            self.room_name = self.get_argument('room')
-            self.user_piece = int(self.get_argument('up'))
-            self.his_piece = self.user_piece == 1 and 2 or 1
-            self.room_name = self.room_name.encode('utf-8')
-            self.mykey = self.room_name+'-'+self.user_piece
-            self.hiskey = self.room_name+'-'+self.his_piece
-        except:
-            return False
-        print u'User '+self.piece+' has entered the room: '+self.room_name
-        if not self.mykey in GameSocketHandler.keys():
-            GameSocketHandler[self.mykey] = self
+
+        self.room_name = self.get_argument('room')
+        self.user_piece = int(self.get_argument('up'))
+        self.his_piece = self.user_piece == 1 and 2 or 1
+        self.room_name = self.room_name.encode('utf-8')
+        self.mykey = "%s-%d" % (self.room_name, self.user_piece)
+        self.hiskey = "%s-%d" % (self.room_name, self.his_piece)
+
+        if not self.room_name in GameSocketHandler.all_rooms:  #第一次进入房间
+            room = GameRoom(self.room_name, self.user_piece)
+            GameSocketHandler.all_rooms[self.room_name] = room
+            if isLog:  print '第一个进入'
+
+        print "User %d has entered the room: %s" % (self.user_piece, self.room_name)
+        if not self.mykey in GameSocketHandler.socket_handlers:
+            GameSocketHandler.socket_handlers[self.mykey] = self
+        self.write_message({'type':'online'})
         return
 
     def on_close(self):
-        print u'User '+self.user_piece+' has left the room: '+self.room_name
-        del GameSocketHandler[self.mykey]
-        if not self.hiskey in GameSocketHandler.socket_handlers.keys():
-            return
-        socket = GameSocketHandler.socket_handlers[self.hiskey]
-        # 给对方发
-        socket.send({'type':'offline'})
+        print 'User %d  has left the room: %s' % (self.user_piece, self.room_name)
+
+        del GameSocketHandler.socket_handlers[self.mykey]
+
+        GameSocketHandler.all_rooms[self.room_name].user_piece_ids.remove(self.user_piece)
+        if not GameSocketHandler.all_rooms[self.room_name].user_piece_ids:  #房间没人了
+            del GameSocketHandler.all_rooms[self.room_name]
+        else: #房间还有一个人，向这个人发通知
+            if not self.hiskey in GameSocketHandler.socket_handlers:
+                return
+            socket = GameSocketHandler.socket_handlers[self.hiskey]
+            # 给对方发
+            socket.write_message({'type':'offline'})
+            GameSocketHandler.all_rooms[self.room_name].gobang.pieces = [([0] * (Gobang.GRID_SIZE+1)) for i in range(Gobang.GRID_SIZE+1)]
+            GameSocketHandler.all_rooms[self.room_name].gobang.last_piece = None
+            GameSocketHandler.all_rooms[self.room_name].status = GameRoom.STATUS_WAITING
+
         return
 
     def on_message(self, message):
-        print 'Websocket receive message: ' + message
+        print 'Room '+self.room_name+' websocket receive message: ' + message
+        msg = json.loads(message)
+        if not 'type' in msg:
+            print 'Error. message has no type filed.'
+            return
+        self._gamemove(msg)
         return
 
     def allow_draft76(self):
         return True
 
+    def _gamemove(self, msg):
+        row = msg.get(u'row')
+        col = msg.get(u'col')
+        room = GameSocketHandler.all_rooms[self.room_name]
+        ret = room.gobang.addPiece(row, col, self.user_piece)
+        if not ret.result:
+            return False
+        else:
+            if room.gobang.isGameOver(row, col):
+                if isLog:  print "Room: %s gameover." % self.room_name.encode('UTF-8')
+                socket = GameSocketHandler.socket_handlers[self.hiskey]
+                socket.write_message({'type':'on_gameover'})
+                self.write_message({'type':'on_gameover'})
+                self.close()
+                socket.close()
+            else:
+                socket = GameSocketHandler.socket_handlers[self.hiskey]
+                socket.write_message({'type':'on_gamemove', 'row':row, 'col':col})
+        return True
+
 class RoomListHandler(web.RequestHandler):
 
     def get(self):
-        self.render('rooms.html', 
-                    all_rooms_count=len(all_rooms),
-                    config=Config,
-                    rooms=all_rooms,
+        self.render('rooms.html',
+                    all_rooms_count = len(GameSocketHandler.all_rooms),
+                    config = Config,
+                    rooms = GameSocketHandler.all_rooms,
                     )
-
-
-def pubContent(content):
-    if isLog: print content
-    zmq_pusher.send_multipart(content)
 
 
 urls = [
         (r"/room-(.{1,200})", EnterRoomHandler),
-        (r"/poll", GamePollHandler),
-        (r"/step", GameStepHandler),
-        (r"/alive", GameAliveHandler),
         (r"/rooms", RoomListHandler),
         (r"/", RoomListHandler),
         (r"/gamesocket", GameSocketHandler),
@@ -265,13 +159,6 @@ settings = dict(
         );
 
 isLog = True
-gobang = Gobang()
-all_rooms = {}
-zmq_addr = 'tcp://127.0.0.1:5005'
-ctx = zmq.Context()
-zmq_pusher = ctx.socket(zmq.PUB)
-zmq_pusher.bind(zmq_addr)
-
 
 def main():
     application = web.Application(urls, **settings)
